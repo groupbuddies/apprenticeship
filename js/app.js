72f96ae6b25e2fee79303c6abf1d5a41fbaaf039
João Justo <jpjustonunes@hotmail.com>
2014-11-19 17:53:31 +0000

    initial commit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+(function(angular) {
+  'use strict';
+
+angular.module('apprenticeship', [])
+  .controller('applicantData', ['$scope', function($scope) {
+    $scope.user = {};
+
+    $scope.update = function(user) {
+      $scope.authorize();
+    };
+
+    $scope.authorize = function() {
+      Trello.authorize({
+        interactive: true,
+        type: "popup",
+        expiration: "never",
+        name: "surveyrequest",
+        persist: "true",
+        success: $scope.onAuthorizeSuccessful(),
+        error: $scope.onFailedAuthorization(),
+        scope: { read: true, write: true}
+      });
+    };
+
+    $scope.onAuthorizeSuccessful = function() {
+      Trello.post("cards", { name: "Card created for test", desc: "this is a test",  idList: "546c84343b42d112498bae36"});
+    };
+
+
+    $scope.onFailedAuthorization = function() {
+      console.log("Authorization failed.");
+    };
+
+  }]);
+})(window.angular);
