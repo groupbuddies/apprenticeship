a46edaaaccbd8eed050d5ea412b27ae904b3d36b
João Justo <jpjustonunes@hotmail.com>
2014-11-21 17:17:45 +0000

    Lots of stuff

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,34 +3,38 @@
 angular.module('apprenticeship', ['ngAnimate'])
   .controller('appController', ['$scope', function($scope) {
     $scope.user = {
-      click: false
+      dev: false,
+      designer: false,
+      devPrograms: false,
+      designerPrograms: false
     };
-
-    $scope.update = function(user) {
-      //$scope.authorize();
-      user.click = !user.click
-    };
-
-    $scope.authorize = function() {
-      Trello.authorize({
-        interactive: true,
-        type: 'popup',
-        expiration: 'never',
-        name: 'surveyrequest',
-        persist: true,
-        success: $scope.onAuthorizeSuccessful(),
-        error: $scope.onFailedAuthorization(),
-        scope: { read: true, write: true}
-      });
-    };
-
-    $scope.onAuthorizeSuccessful = function() {
-      Trello.post("cards", { name: "Card created for test", desc: "this is a test",  idList: "546c84343b42d112498bae36"});
-    };
-
-
-    $scope.onFailedAuthorization = function() {
-      console.log('Authorization failed.');
-    };
-
+  //
+  //   $scope.update = function(user) {
+  //     // $scope.authorize();
+  //     // user.click = !user.click;
+  //   };
+  //
+  //   $scope.authorize = function() {
+  //     Trello.authorize({
+  //       interactive: true,
+  //       type: 'popup',
+  //       expiration: 'never',
+  //       name: 'surveyrequest',
+  //       persist: true,
+  //       success: $scope.onAuthorizeSuccessful(),
+  //       error: $scope.onFailedAuthorization(),
+  //       scope: { read: true, write: true}
+  //     });
+  //   };
+  //
+  //   $scope.onAuthorizeSuccessful = function() {
+  //     var description = $scope.user.name;
+  //     Trello.post("cards", { name: "Card created for test", desc: description,  idList: "546c84343b42d112498bae36"});
+  //   };
+  //
+  //
+  //   $scope.onFailedAuthorization = function() {
+  //     console.log('Authorization failed.');
+  //   };
+  //
   }]);
