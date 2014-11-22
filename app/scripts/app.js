6779c27b04f5a227b37cf0e658c16de33652d78b
João Justo <jpjustonunes@hotmail.com>
2014-11-22 16:03:53 +0000

    Added ui-router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,40 +1,34 @@
 'use strict';
 
-angular.module('apprenticeship', ['ngAnimate'])
+var modules = ['ui.router'];
+
+var apprenticeship = angular.module('apprenticeship', modules)
   .controller('appController', ['$scope', function($scope) {
-    $scope.user = {
-      dev: false,
-      designer: false,
-      devPrograms: false,
-      designerPrograms: false
-    };
-  //
-  //   $scope.update = function(user) {
-  //     // $scope.authorize();
-  //     // user.click = !user.click;
-  //   };
-  //
-  //   $scope.authorize = function() {
-  //     Trello.authorize({
-  //       interactive: true,
-  //       type: 'popup',
-  //       expiration: 'never',
-  //       name: 'surveyrequest',
-  //       persist: true,
-  //       success: $scope.onAuthorizeSuccessful(),
-  //       error: $scope.onFailedAuthorization(),
-  //       scope: { read: true, write: true}
-  //     });
-  //   };
-  //
-  //   $scope.onAuthorizeSuccessful = function() {
-  //     var description = $scope.user.name;
-  //     Trello.post("cards", { name: "Card created for test", desc: description,  idList: "546c84343b42d112498bae36"});
-  //   };
-  //
-  //
-  //   $scope.onFailedAuthorization = function() {
-  //     console.log('Authorization failed.');
-  //   };
-  //
-  }]);
+  }])
+
+apprenticeship.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise("/");
+
+  $stateProvider
+    .state('/', {
+      url: "/",
+      templateUrl: "views/index.html"
+    })
+    .state('developer_programs', {
+      url: "/dev_programs",
+      templateUrl: "views/dev_programs.html"
+    })
+    .state('designer_programs', {
+      url: "/designer_programs",
+      templateUrl: "views/designer_programs.html"
+    })
+    .state('developer_apply', {
+      url: "/developer_apply",
+      templateUrl: "views/applicant_form_dev.html"
+    })
+    .state('designer_apply', {
+      url: "/designer_apply",
+      templateUrl: "views/applicant_form_designer.html"
+    });
+});
