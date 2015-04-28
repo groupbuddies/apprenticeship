1827bb2f65d9457a4735979212a8bcacc1db7f88
Gabriel Poça <gabrielpoca@gmail.com>
2015-04-28 15:47:47 +0100

    Use push state and improves js

--- src/app/index.js
+++ src/app/index.js
@@ -1,48 +1,46 @@
-'use strict';
+(function() {
+  'use strict';
 
-var modules = ['ngAnimate', 'ngTouch', 'ui.router'];
+  angular
+    .module('apprenticeship', ['ngAnimate', 'ngTouch', 'ui.router'])
+    .controller('appController', function($scope) {})
+    .run(autoScroll)
+    .config(router);
 
-var scrollToTop = function () {
-  $('html, body').animate({ scrollTop: 0 }, 10);
-};
-
-var apprenticeship = angular.module('apprenticeship', modules);
-
-apprenticeship.controller('appController', ['$scope', function($scope) {
-  }]);
-
-apprenticeship.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/');
-
-  $stateProvider
-    .state('/', {
-      url: '/',
-      templateUrl: 'app/views/index.html',
-      onEnter: scrollToTop
-    })
-    .state('developer', {
-      url: '/developer',
-      templateUrl: 'app/views/dev_programs.html',
-      onEnter: scrollToTop
-    })
-    .state('designer', {
-      url: '/designer',
-      templateUrl: 'app/views/designer_programs.html',
-      onEnter: scrollToTop
-    })
-    .state('developer_apply', {
-      url: '/developer/apply',
-      templateUrl: 'app/views/applicant_form_dev.html',
-      onEnter: scrollToTop
-    })
-    .state('designer_apply', {
-      url: '/designer/apply',
-      templateUrl: 'app/views/applicant_form_designer.html',
-      onEnter: scrollToTop
-    })
-    .state('success', {
-      url: '/success',
-      templateUrl: 'app/views/success.html',
-      onEnter: scrollToTop
+  function autoScroll($rootScope, $anchorScroll) {
+    $rootScope.$on('$locationChangeSuccess', function() {
+      $anchorScroll();
     });
-});
+  }
+
+  function router($stateProvider, $urlRouterProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('/', {
+        url: '/',
+        templateUrl: 'app/views/index.html',
+      })
+      .state('developer', {
+        url: '/developer',
+        templateUrl: 'app/views/dev_programs.html',
+      })
+      .state('designer', {
+        url: '/designer',
+        templateUrl: 'app/views/designer_programs.html',
+      })
+      .state('developer_apply', {
+        url: '/developer/apply',
+        templateUrl: 'app/views/applicant_form_dev.html',
+      })
+      .state('designer_apply', {
+        url: '/designer/apply',
+        templateUrl: 'app/views/applicant_form_designer.html',
+      })
+      .state('success', {
+        url: '/success',
+        templateUrl: 'app/views/success.html',
+      });
+  }
+})();
