d78d0ed0528fa35b42637e20e0b512471af2dece
Gabriel Poça <gabrielpoca@gmail.com>
2015-06-01 10:38:28 +0100

    Add capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,10 @@
+set :application, 'apprenticeship.subvisual.co'
+set :repo_url, 'git@github.com:groupbuddies/apprenticeship.git'
+set :stage, :production
+set :branch, :master
+set :linked_dirs, %w(log)
+
+set :scm, :middleman
+set :format, :pretty
+set :log_level, :debug
+set :pty, true
