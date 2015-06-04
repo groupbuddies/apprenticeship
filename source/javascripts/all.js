2ccbfec136fdad98c38f4cc801b4b8dd0fcc81e7
Gabriel Poça <gabrielpoca@gmail.com>
2015-06-04 15:46:46 +0100

    Add balloon and update clouds

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,2 +1,8 @@
 //= require jquery
 //= require javascripts
+
+$(function() {
+  $('.CollapsedBody').on('click', '.CollapsedBody-header', function(e) {
+    $(this).parent().toggleClass('CollapsedBody--open');
+  });
+});
