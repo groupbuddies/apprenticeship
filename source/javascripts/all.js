81a9daaa460c1269dacf67d42d593544c90be67f
Gabriel Poça <gabrielpoca@gmail.com>
2015-06-05 17:35:01 +0100

    Only hide faq in mobile

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -7,30 +7,37 @@ $(function() {
     $(this).parent().toggleClass('CollapsedBody--open');
   });
 
-  var faqSection = $('#faq');
-  var faqLearnMoreSection = $('#faq-learn');
-  var faqSectionHeight = faqSection.height();
+  var tabletBreakpoint = 768;
 
-  faqSection.css({
-    height: 0,
-    visibility: 'hidden'
-  });
+  if ($(window).width() <= tabletBreakpoint) {
+    var faqSection = $('#faq');
+    var faqLearnMoreSection = $('#faq-learn');
+    var faqSectionHeight = faqSection.height();
 
-  $('.hidden-section-trigger').on('click', function() {
-    Velocity(faqLearnMoreSection, {
-      opacity: 0,
-      height: 0,
-      complete: function() {
-        Velocity(faqSection, {
-          height: faqSectionHeight,
-          complete: function() {
-            faqSection.css({
-              visibility: 'visible',
-              height: 'auto'
-            });
-          }
-        });
-      }
+    var hideSection = function() {
+      console.log('complete');
+      faqSection.css({
+        height: 0,
+        visibility: 'hidden'
+      });
+    };
+
+    var showSection = function() {
+      faqSection.css({
+        visibility: 'visible',
+        height: 'auto'
+      });
+    };
+
+    hideSection();
+    $('.hidden-section-trigger').on('click', function() {
+      Velocity(faqLearnMoreSection, {
+        opacity: 0,
+        height: 0,
+        complete: function() {
+          Velocity(faqSection, {height: faqSectionHeight }, { complete: showSection });
+        }
+      });
     });
-  });
+  }
 });
