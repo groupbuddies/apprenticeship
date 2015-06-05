f40f74367d9cfca57f7c428fda10a197a0867005
Gabriel Poça <gabrielpoca@gmail.com>
2015-06-05 16:47:35 +0100

    Add hidden faq with show animation

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,8 +1,36 @@
+//= require velocity
 //= require jquery
 //= require javascripts
 
 $(function() {
-  $('.CollapsedBody').on('click', '.CollapsedBody-header', function(e) {
+  $('.CollapsedBody').on('click', '.CollapsedBody-header', function() {
     $(this).parent().toggleClass('CollapsedBody--open');
+  });
+
+  var faqSection = $('#faq');
+  var faqLearnMoreSection = $('#faq-learn');
+  var faqSectionHeight = faqSection.height();
+
+  faqSection.css({
+    height: 0,
+    visibility: 'hidden'
+  });
+
+  $('.hidden-section-trigger').on('click', function() {
+    Velocity(faqLearnMoreSection, {
+      opacity: 0,
+      height: 0,
+      complete: function() {
+        Velocity(faqSection, {
+          height: faqSectionHeight,
+          complete: function() {
+            faqSection.css({
+              visibility: 'visible',
+              height: 'auto'
+            });
+          }
+        });
+      }
+    });
   });
 });
