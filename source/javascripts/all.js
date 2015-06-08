8129792528b10b489505470659306a793cd2617d
Gabriel Poça <gabrielpoca@gmail.com>
2015-06-08 11:11:37 +0100

    Rename CollapsedBody with CollapsedSection. Fix FAQ.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,42 +2,5 @@
 //= require jquery
 //= require javascripts
 
-$(function() {
-  $('.CollapsedBody').on('click', '.CollapsedBody-header', function() {
-    $(this).parent().toggleClass('CollapsedBody--open');
-  });
-
-  var tabletBreakpoint = 768;
-
-  if ($(window).width() <= tabletBreakpoint) {
-    var faqSection = $('#faq');
-    var faqLearnMoreSection = $('#faq-learn');
-    var faqSectionHeight = faqSection.height();
-
-    var hideSection = function() {
-      console.log('complete');
-      faqSection.css({
-        height: 0,
-        visibility: 'hidden'
-      });
-    };
-
-    var showSection = function() {
-      faqSection.css({
-        visibility: 'visible',
-        height: 'auto'
-      });
-    };
-
-    hideSection();
-    $('.hidden-section-trigger').on('click', function() {
-      Velocity(faqLearnMoreSection, {
-        opacity: 0,
-        height: 0,
-        complete: function() {
-          Velocity(faqSection, {height: faqSectionHeight }, { complete: showSection });
-        }
-      });
-    });
-  }
-});
+//= require faq
+//= require collapsed_section
