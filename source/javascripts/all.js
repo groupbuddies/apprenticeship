557f71d97be25f8144946097e6f7bb582ad63a40
Gabriel Poça <gabrielpoca@gmail.com>
2015-06-24 14:40:10 +0100

    Add balloon animation using velocity

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,6 +1,44 @@
-//= require velocity
+//= require velocity/velocity
 //= require jquery
 //= require blue
 
 //= require faq
 //= require collapsed_section
+
+$(function() {
+  var el = $('.Panel-balloonWrapper');
+
+  var documentWidth = $('body').width();
+
+  var x = 0;
+
+  function up(xDistance, cb) {
+    x -= xDistance;
+
+    Velocity(el, {
+      translateX: x,
+      translateY: -200,
+    }, {
+      duration: 10000,
+      complete: cb
+    }, 'easeOut');
+  }
+
+  function down(xDistance, cb) {
+    x -= xDistance;
+
+    Velocity(el, {
+      translateX: x,
+      translateY: 0
+    }, {
+      duration: 10000,
+      complete: cb
+    }, 'easeInSine');
+  }
+
+  up(documentWidth / 4, function() {
+    down(documentWidth / 4, function() {
+      up(documentWidth / 2);
+    });
+  });
+});
